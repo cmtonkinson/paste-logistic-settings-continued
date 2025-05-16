8b3e098b1fa221fb780aed45be101c3c0f0e43e3
Chris Tonkinson <chris@tonkinson.com>
2025-05-16 11:04:37 -0400

    Hide experimental settings

--- settings.lua
+++ settings.lua
@@ -37,50 +37,50 @@ data:extend({
     default_value = 1,
     minimum_value = 1,
   },
-  {
-    type = "bool-setting",
-    name = "paste-logistic-settings-continued-enable-ghost-placement",
-    order = "b[a]",
-    setting_type = "runtime-per-user",
-    default_value = false
-  },
-  {
-    type = "string-setting",
-    name = "paste-logistic-settings-continued-ghost-direction",
-    order = "b[b]",
-    setting_type = "runtime-per-user",
-    allowed_values = { "north", "east", "south", "west" },
-    default_value = "south"
-  }
-  ,
-  {
-    type = "string-setting",
-    name = "paste-logistic-settings-continued-ghost-input-inserter",
-    order = "b[c]",
-    setting_type = "runtime-per-user",
-    allowed_values = {
-      "burner-inserter",
-      "inserter",
-      "fast-inserter",
-      "long-handed-inserter",
-      "stack-inserter",
-      "bulk-inserter",
-    },
-    default_value = "bulk-inserter",
-  },
-  {
-    type = "string-setting",
-    name = "paste-logistic-settings-continued-ghost-output-inserter",
-    order = "b[d]",
-    setting_type = "runtime-per-user",
-    allowed_values = {
-      "burner-inserter",
-      "inserter",
-      "fast-inserter",
-      "long-handed-inserter",
-      "stack-inserter",
-      "bulk-inserter",
-    },
-    default_value = "fast-inserter",
-  },
+--  {
+--    type = "bool-setting",
+--    name = "paste-logistic-settings-continued-enable-ghost-placement",
+--    order = "b[a]",
+--    setting_type = "runtime-per-user",
+--    default_value = false
+--  },
+--  {
+--    type = "string-setting",
+--    name = "paste-logistic-settings-continued-ghost-direction",
+--    order = "b[b]",
+--    setting_type = "runtime-per-user",
+--    allowed_values = { "north", "east", "south", "west" },
+--    default_value = "south"
+--  }
+--  ,
+--  {
+--    type = "string-setting",
+--    name = "paste-logistic-settings-continued-ghost-input-inserter",
+--    order = "b[c]",
+--    setting_type = "runtime-per-user",
+--    allowed_values = {
+--      "burner-inserter",
+--      "inserter",
+--      "fast-inserter",
+--      "long-handed-inserter",
+--      "stack-inserter",
+--      "bulk-inserter",
+--    },
+--    default_value = "bulk-inserter",
+--  },
+--  {
+--    type = "string-setting",
+--    name = "paste-logistic-settings-continued-ghost-output-inserter",
+--    order = "b[d]",
+--    setting_type = "runtime-per-user",
+--    allowed_values = {
+--      "burner-inserter",
+--      "inserter",
+--      "fast-inserter",
+--      "long-handed-inserter",
+--      "stack-inserter",
+--      "bulk-inserter",
+--    },
+--    default_value = "fast-inserter",
+--  },
 })
